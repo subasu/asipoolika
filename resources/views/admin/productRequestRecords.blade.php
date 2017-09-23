fb0df4e245e435850ebbef8d9a08c6a97ae5cee5
mizogii <atefehkianfar1@gmail.com>
2017-09-23 20:42:39 +0330

    change product request view

--- resources/views/admin/productRequestRecords.blade.php
+++ resources/views/admin/productRequestRecords.blade.php
@@ -10,14 +10,14 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title" style="direction: rtl; font-size: 20px;">رد درخواست</h4>
+                    <h4 class="modal-title" style="direction: rtl;">رد درخواست</h4>
                 </div>
                 <div class="modal-body">
-                    <h4 style="direction: rtl; font-size: 20px;">لطفا دلیل رد درخواست را بطور کامل تایپ کنید.</h4>
+                    <h4 style="direction: rtl;">لطفا دلیل رد درخواست را بطور کامل تایپ کنید.</h4>
                     <textarea class="form-control" id="whyNot" name="whyNot" placeholder=""></textarea>
                 </div>
                 <div class="modal-footer">
-                    <button style="margin-left:40%; width: 30%;font-size: 20px;" type="button" class="btn btn-primary" id="sub" data-dismiss="modal">ثبت</button>
+                    <button style="margin-left:40%; width: 30%;" type="button" class="btn btn-primary" id="sub" data-dismiss="modal">ثبت</button>
                 </div>
             </div>
             <input type="hidden" id="token" value="{{ csrf_token() }}">
@@ -43,13 +43,12 @@
                 <div class="clearfix"></div>
             </div>
             <div class="x_content">
-                <table style="direction:rtl;text-align: center" id="table" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
+                <table style="direction:rtl;text-align: center;font-size: 16px;" id="table" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
                     <thead>
                     <tr>
                         <th style="text-align: center ;">شماره</th>
                         <th style="text-align: center ;">عنوان درخواست</th>
                         <th style="text-align: center ;">مقدار</th>
-                        <th style="text-align: center ;">توضیحات</th>
                         <th style="text-align: center ;">نرخ (به تومان)</th>
                         <th style="text-align: center ;">قیمت</th>
                         <th style="text-align: center ;">عملیات</th>
@@ -64,11 +63,12 @@
                             <tr>
                                 <td>{{$requestRecord->id}}</td>
                                 <td>{{$requestRecord->title}}</td>
-                                <td>{{$requestRecord->count}}</td>
-                                <td>{{$requestRecord->description}}</td>
+                                <td>{{$requestRecord->count}} {{$requestRecord->unit_count}}</td>
                                 <td><input type="text" class="form-control" id="rate"  name="rate"/></td>
                                 <td><input type="text" class="form-control" id="price" name="price"/></td>
-                                <td><input id="acceptRequest" content="{{$requestRecord->id}}" name="{{$requestRecord->request_id}}" type="button" class="btn btn-success" required value="پیگیری درخواست" /> <input id="refuseRequest" content="{{$requestRecord->id}}" name="{{$requestRecord->request_id}}" type="button" class="btn btn-danger"  required value="رد کردن درخواست" /></td>
+                                <td><button class="btn btn-link btn-round" data-toggle="tooltip" title="{{$requestRecord->description}}"> توضیحات
+                                </button>
+                                <input id="acceptRequest" content="{{$requestRecord->id}}" name="{{$requestRecord->request_id}}" type="button" class="btn btn-success" required value="پیگیری" /> <input id="refuseRequest" content="{{$requestRecord->id}}" name="{{$requestRecord->request_id}}" type="button" class="btn btn-danger"  required value="رد کردن" /></td>
                             </tr>
                         @endforeach
                     {{--</form>--}}
